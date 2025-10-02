8bbdd2f88df372c7dd86de35b2c0d50aad3f2935
XrayFlix <145455567+XrayFlix@users.noreply.github.com>
2025-10-02 13:24:42 +0300

    Update ProductItem.jsx

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function ProductItem({ product, onRemove }) {
   return (
     <li>
-      {product.important && ""} {product.title} ({product.category}){" "}
+      {product.important && "*"} {product.title} ({product.category}){" "}
       <button onClick={() => onRemove(product.id)}>Удалить</button>
     </li>
   );
 }
+
 
 export default ProductItem;
